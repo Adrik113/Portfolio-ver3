9b717de591478b097e07a50f88e0f4712c8917f8
Adrik113 <adrwar3273@students.ecpi.edu>
2025-10-07 15:24:15 -0400

    fix tailwind 4 config for vercel

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+};
